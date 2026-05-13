00054d8e72812ede88d6d70bd035a1274cf15df9
CecyTeix <103583726+CecyTeix@users.noreply.github.com>
2026-05-13 13:40:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const formulario = document.querySelector('#formulario')
+const nome = document.querySelector('#nome')
+const marca = document.querySelector('#marca')
+const preco = document.querySelector('#preco')
+const qtde_estoque = document.querySelector('#qtde_estoque')
+const container = document.querySelector('#container')
+const card = document.querySelector('#card')
+
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault() //Impede que a página atualize
+    const novoCard = document.createElement("div")
+    novoCard.className ="card"
+
+    const novoNome = document.createElement("h2")
+    novoNome.textContent = nome.value
+
+    const novaMarca = document.createElement("p")
+    novaMarca.textContent = `Marca: ${marca.value}`
+
+    const novoPreco = document.createElement("p")
+    novoPreco.textContent = `Preço: ${preco.value}`
+
+    const novaQtd = document.createElement("p")
+    novaQtd.textContent = `Quantidade em Estoque: ${qtde_estoque.value}`
+
+    const botaoExcluir = document.createElement("button")
+    botaoExcluir.textContent = "Excluir"
+
+    botaoExcluir.addEventListener('click', () => {
+        novoCard.remove()
+    })
+
+    novoCard.append(novoNome, novaMarca, novoPreco, novaQtd, botaoExcluir)
+    container.append(novoCard)
+    
+    formulario.reset()
+    nome.focus()
+})
